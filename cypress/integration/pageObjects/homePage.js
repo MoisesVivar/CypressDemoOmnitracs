9ab5f31a7d7bee21aa74f3fd0728953d233b19aa
Moisés Vivar <moises.vivar@omnitracs.com>
2021-06-22 14:23:11 -0500

    fixing homePage methods

--- cypress/integration/pageObjects/homePage.js
+++ cypress/integration/pageObjects/homePage.js
@@ -15,18 +15,18 @@ export default class homePage{
         return cy.get(".list-group > [href='./basic-radiobutton-demo.html']")
     }
     static DropdownListButton(){
-        return cy.get("list-group > [href='./basic-select-dropdown-demo.html']")
+        return cy.get(".list-group > [href='./basic-select-dropdown-demo.html']")
     }
     static JavascriptAlertsButton(){
-        return cy.get("list-group > [href='./javascript-alert-box-demo.html']")
+        return cy.get(".list-group > [href='./javascript-alert-box-demo.html']")
     }
     static WindowPopupButton(){
-        return cy.get("list-group > [href='./window-popup-modal-demo.html']")
+        return cy.get(".list-group > [href='./window-popup-modal-demo.html']")
     }
     static BootstrapAlertsButton(){
-        return cy.get("list-group > [href='./bootstrap-alert-messages-demo.html']")
+        return cy.get(".list-group > [href='./bootstrap-alert-messages-demo.html']")
     }
     static BootstrapModalsButton(){
-        return cy.get("list-group > [href='./bootstrap-modal-demo.html']")
+        return cy.get(".list-group > [href='./bootstrap-modal-demo.html']")
     }
 }
